14e0159a8d59da6f0c9ccb769707750ba56c5fe3
vakaakhila5-sudo <vakaakhila5@gmail.com>
2026-06-24 13:17:59 +0530

    Add files via upload

---
+++ Dn-java-fse/Mockito_Exercises/Exercise1_MockingAndStubbing/src/main/java/com/cognizant/MyService.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class MyService {
+    private final ExternalApi externalApi;
+
+    public MyService(ExternalApi externalApi) {
+        this.externalApi = externalApi;
+    }
+
+    public String fetchData() {
+        String data = externalApi.getData();
+        return "Service received: " + data;
+    }
+}
